1fc69f172f92ecae28dc9da30c9c77f645f97190
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-02-17 11:46:59 +0100

    image added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <link rel="shortcut icon" type="image/png" sizes="512x512" href="{{ asset('/img/favicon.png') }}">
+    <title>{{ ('Springfield Elementary School') }}</title>
     <title>@yield('title') | {{ config('app.name') }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 4.1.1 -->
